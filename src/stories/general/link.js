862ed144b32fecc5073e9978a1d17491e2dbb20c
Bas <bas@Bass-iMac.local>
2018-08-06 18:00:36 +0700

    Add link component

---
+++ src/stories/general/link.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import {withInfo} from '@storybook/addon-info'
+import {StoryGeneral} from './../main'
+import {Link} from './../../component'
+
+const {storiesOf, defaultInfo} = StoryGeneral
+storiesOf.add(
+  'Link',
+  withInfo({...defaultInfo, source: false, text: `<h4>Source: <<b>Link</b>>Children</<b>Link</b>></h4>`})(() => [
+    <div>
+      <h1 className='heading'>Link</h1>
+      <div className='haft'>
+        <div className='grid2'>
+          <div className='group'>
+            <h3>Primary</h3>
+            <div className='inlineGroup'>
+              <div>
+                <Link type='primary'>Read more...</Link>
+              </div>
+            </div>
+          </div>
+          <div className='group'>
+            <h3>Success</h3>
+            <div className='inlineGroup'>
+              <div>
+                <Link type='success'>Read more...</Link>
+              </div>
+            </div>
+          </div>
+          <div className='group'>
+            <h3>Danger</h3>
+            <div className='inlineGroup'>
+              <div>
+                <Link type='danger'>Read more...</Link>
+              </div>
+            </div>
+          </div>
+          <div className='group'>
+            <h3>Warning</h3>
+            <div className='inlineGroup'>
+              <div>
+                <Link type='warning'>Read more...</Link>
+              </div>
+            </div>
+          </div>
+          <div className='group'>
+            <h3>Back</h3>
+            <div className='inlineGroup'>
+              <div>
+                <Link type='back'>Go back</Link>
+              </div>
+            </div>
+          </div>
+          <div className='group'>
+            <h3>Icon</h3>
+            <div className='inlineGroup'>
+              <div>
+                <Link type='back' icon={<i className='iconfont icon-home' />}>
+                  Home page
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ])
+)
